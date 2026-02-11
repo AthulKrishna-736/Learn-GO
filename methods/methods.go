faa46c704aa2bb4a0190438c41b3ca2a3a085632
Athul krishna K S <athulkrishna736@gmail.com>
2026-02-11 23:58:57 +0530

    methods deep covered

--- methods/methods.go
+++ methods/methods.go
@@ -12,8 +12,21 @@ func (u User) greet() {
 	fmt.Println("Hello ,", u.name)
 }
 
+type Sample struct {
+	c int
+}
+
+func (s Sample) hello(a, b int) (int, string) {
+	return a + b + s.c, "sample output"
+}
+
 func main() {
 	user1 := User{"sample", 10, true}
 	fmt.Println("user1 details: ", user1, user1.Active, user1.age, user1.name)
 	user1.greet()
+
+	sample1 := Sample{10}
+
+	res, str := sample1.hello(19, 2)
+	fmt.Println("sample res: ", res, str)
 }
