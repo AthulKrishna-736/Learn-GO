770394ce2d7dfde37cd41d2e6898325faf404eac
AthulKrishna-736 <athulkrishna736@gmail.com>
2026-02-24 17:47:31 +0530

    covered json basics from encoding/json library

--- web/web.go
+++ web/web.go
@@ -1,21 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 )
 
 const url = "https://lco.dev"
 
-func main() {
-	fmt.Println("sample web requests")
+type User struct {
+	Name   string   `json:"name"`
+	Age    int      `json:"age"`
+	Status bool     `json:"status"`
+	Fruits []string `json:"fruits"`
+}
 
-	res, err := http.Get(url)
+func main() {
+	// fmt.Println("sample web requests")
+
+	// res, err := http.Get(url)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("res: ", res)
+	// defer res.Body.Close() // should close res as callers responsibility
+
+	a := User{"Sample", 10, false, []string{"apple", "mango", "banana"}}
+
+	res, err := json.Marshal(a)
 
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
 	}
 
-	fmt.Println("res: ", res)
-	defer res.Body.Close() // should close res as callers responsibility
+	fmt.Println("json: ", string(res))
+
+	var b User
+
+	err1 := json.Unmarshal(res, &b)
+
+	if err1 != nil {
+		fmt.Println(err1.Error())
+	}
+
+	fmt.Println("Struct: ", b)
+
+	isValid := json.Valid(res)
+	fmt.Println("is proper json: ", isValid)
 }
