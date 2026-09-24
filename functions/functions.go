3d0fc270e679d658f511e18e8dfc2b9896d5128c
AthulKrishna-736 <athulkrishna736@gmail.com>
2026-09-24 18:36:40 +0530

    learning variadic funciton

--- functions/functions.go
+++ functions/functions.go
@@ -18,6 +18,15 @@ func add(x int, y int) (res int, text string) {
 	return
 }
 
+func variadicFunction(nums ...int) int {
+	var sum int
+	for _, val := range nums {
+		sum += val
+	}
+
+	return sum
+}
+
 func main() {
 	myMessage()
 	hello("athul", 10)
@@ -26,5 +35,12 @@ func main() {
 
 	a, b := add(2, 3)
 	fmt.Println("total values: ", a, b)
+
+	res := variadicFunction(1, 2, 3, 4)
+	fmt.Printf("\nvariadic 1: %d", res)
+
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	res1 := variadicFunction(nums...)
+	fmt.Printf("\nvariadic 2: %d", res1)
 
 }
